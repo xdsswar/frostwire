0124b62403842503ef95fb1ad6ba897c835d7b25
frostwire <contact@frostwire.com>
2019-05-07 01:56:15 -0600

    [android] python initialization test (broken)

--- android/jni/python_bridge.c
+++ android/jni/python_bridge.c
@@ -1,5 +1,11 @@
 #include "com_frostwire_android_python_PythonBridge.h"
+#include <Python.h>
 
 JNIEXPORT jint JNICALL Java_com_frostwire_android_python_PythonBridge_square(JNIEnv *jniEnv, jclass clazz, jint n) {
+  Py_SetProgramName(argv[0]);  /* optional but recommended */
+  Py_Initialize();
+  PyRun_SimpleString("from time import time,ctime\n"
+                     "print 'Today is',ctime(time())\n");
+  Py_Finalize();
   return n*n;
 }
