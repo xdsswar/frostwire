73ed3050063efe6206e05ec0fafefc58683d986a
frostwire <contact@frostwire.com>
2019-05-07 19:58:08 -0600

    [android] compile fix

--- android/jni/python_bridge.c
+++ android/jni/python_bridge.c
@@ -2,7 +2,7 @@
 #include <Python.h>
 
 JNIEXPORT jint JNICALL Java_com_frostwire_android_python_PythonBridge_square(JNIEnv *jniEnv, jclass clazz, jint n) {
-  Py_SetProgramName(argv[0]);  /* optional but recommended */
+  Py_SetProgramName("frostwire python bridge");  /* optional but recommended */
   Py_Initialize();
   PyRun_SimpleString("from time import time,ctime\n"
                      "print 'Today is',ctime(time())\n");
